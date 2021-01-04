2fe34ae381c26b647f49787792dc8db868d621b0
Gareth Brown <gareth.brown@savant.co.uk>
2021-01-04 10:20:15 +0000

    Should be public static

--- TODOCommentMapper/Program.cs
+++ TODOCommentMapper/Program.cs
@@ -8,9 +8,9 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace TODOCommentMapper
 {
-	class Program
+	public static class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			var solutionFilePath = @"..\..\..\TODOCommentMapper.sln";
 
